4cc20fd7cad15ce967f040a9d658f74a77c1d6ff
mgiolai <mgiolai.ac@gmail.com>
2024-01-09 11:06:57 +0100

    Update 002_PST_map-air-seq-reads.sh
    Updated filtering.

--- air-seq_read-mapping-to-PHIbase-and-PST/002_PST_map-air-seq-reads.sh
+++ air-seq_read-mapping-to-PHIbase-and-PST/002_PST_map-air-seq-reads.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=2   # The number of cores
+#SBATCH --mem=8G  # The memory per core
 #SBATCH --partition=long
-#SBATCH --output=/mnt/shared/scratch/mgiolai/air-seq/job-outs/slurm-%J.out
-#SBATCH --error=/mnt/shared/scratch/mgiolai/air-seq/job-outs/slurm-%J.err
+#SBATCH --output=/mnt/shared/scratch/mgiolai/air-seq/job-outs/slurm-%J_map.out
+#SBATCH --error=/mnt/shared/scratch/mgiolai/air-seq/job-outs/slurm-%J_map.err
+#SBATCH --mail-user=mgiolai.ac@gmail.com
+#SBATCH --mail-type=END,FAIL
 
 references=/mnt/shared/scratch/mgiolai/air-seq/references
-reads=/mnt/shared/scratch/mgiolai/air-seq/reads
-ASvsPHI=/mnt/shared/scratch/mgiolai/air-seq/bwa_airseq_vs_phibase4-12
+reads=/mnt/shared/scratch/mgiolai/air-seq/air-seq_JIC/read-data/reads_renamed_merged_fp_cdh_fa
 
 source activate bwa
 cd ${references}
-#Index PST genome, which has to be downloaded and extracted first
-srun bwa index Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa
-conda deactivate
+#Index PST genome
+#srun bwa index Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa
+
 
 #Map the Air-seq reads to the PST reference
-mkdir -p /mnt/shared/scratch/mgiolai/air-seq/bwa_airseq_vs_PST
-cd /mnt/shared/scratch/mgiolai/air-seq/bwa_airseq_vs_PST
-for file in $(ls ${reads}/*.fp.cd.fa.gz); do
+mkdir -p /mnt/shared/scratch/mgiolai/air-seq/air-seq_JIC/bwa_airseq_vs_PST
+cd /mnt/shared/scratch/mgiolai/air-seq/air-seq_JIC/bwa_airseq_vs_PST
+for file in $(ls ${reads}/*.fp.cd.fa.gz | grep "airseq_JIC_sample"); do
 	prefix=$(echo ${file} | xargs -L1 basename | sed 's/.fp.cd.fa.gz//')
 	echo ${prefix}
 	srun gunzip -c ${file} > ${prefix}.tmp
 	source activate bwa
-	srun bwa mem ${references}/Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa ${prefix}.tmp > ${prefix}.sam
+	srun bwa mem -t 4 ${references}/Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa ${prefix}.tmp > ${prefix}.sam
 	conda deactivate
-	srun rm ${prefix}.tmp
-	srun filtersam -i 95 -m 95 -o ${prefix}.95pident.95pmatch.sam ${prefix}.sam
-	source activate samtools
-	srun samtools view -S -b ${prefix}.95pident.95pmatch.sam > ${prefix}.95pident.95pmatch.bam
+	srun filtersam -i 95 -o ${prefix}.tmp1.sam ${prefix}.sam
+	srun filtersam -m 95 -o ${prefix}.tmp2.sam ${prefix}.tmp1.sam
+	source activate bwa
+	srun samtools view -b -q 1 ${prefix}.tmp2.sam > ${prefix}.95pident.95pmatch.bam
 	srun samtools sort ${prefix}.95pident.95pmatch.bam -o ${prefix}.95pident.95pmatch.sorted.bam
 	srun samtools index ${prefix}.95pident.95pmatch.sorted.bam
 	srun bcftools mpileup -f ${references}/Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa ${prefix}.95pident.95pmatch.sorted.bam > ${prefix}.95pident.95pmatch.raw_calls.bcf
@@ -36,30 +37,6 @@ for file in $(ls ${reads}/*.fp.cd.fa.gz); do
 	srun bcftools filter -i "QUAL>20 && DP>20" ${prefix}.95pident.95pmatch.calls.vcf | bcftools view -g ^miss > ${prefix}.95pident.95pmatch.filtered.calls.vcf
 	srun bcftools stats ${prefix}.95pident.95pmatch.filtered.calls.vcf | grep "TSTV" > ${prefix}.95pident.95pmatch.filtered.calls.TSTV.txt
 	conda deactivate
+	srun rm ${prefix}.tmp ${prefix}.tmp1.sam ${prefix}.tmp2.sam ${prefix}.95pident.95pmatch.bam ${prefix}.95pident.95pmatch.sorted.bam ${prefix}.95pident.95pmatch.sorted.bam.bai
+	srun gzip --best ${prefix}.sam
 done
-
-#Map the reads that mapped to the PST genome in the PHI-baes database selectively
-prefix="phibase.filtered.PST"
-echo ${prefix}
-srun zcat ${ASvsPHI}/*JIC*95*95*.sam.gz | grep -E "taxid\|27350\|" | grep -v "^@" | awk '{print $1}' > names.txt
-srun zcat ${reads}/*JIC*.fa.gz | grep -A1 -f names.txt --no-group-separator > ${prefix}.reads.fa
-source activate bwa
-srun bwa mem ${references}/Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa ${prefix}.reads.fa > ${prefix}.sam
-conda deactivate
-srun filtersam -i 95 -m 95 -o ${prefix}.sam ${prefix}.sam
-source activate samtools
-srun samtools view -S -b ${prefix}.sam > ${prefix}.bam
-srun samtools sort ${prefix}.bam -o ${prefix}.sorted.bam
-srun samtools index ${prefix}.sorted.bam
-srun bcftools mpileup -f ${references}/Puccinia_striiformis.PST-130_1.0.dna.toplevel.fa ${prefix}.sorted.bam > ${prefix}.raw_calls.bcf
-srun bcftools call -v -m ${prefix}.raw_calls.bcf > ${prefix}.calls.vcf
-srun bcftools filter -i "QUAL>20 && DP>20" ${prefix}.calls.vcf | bcftools view -g ^miss > ${prefix}.filtered.calls.vcf
-srun bcftools stats ${prefix}.filtered.calls.vcf | grep "TSTV" > ${prefix}.filtered.calls.TSTV.txt
-conda deactivate
-
-#Can we find P. triticina reads?
-prefix="phibase.filtered.PT"
-mkdir -p /mnt/shared/scratch/mgiolai/air-seq/bwa_airseq_vs_PT
-cd /mnt/shared/scratch/mgiolai/air-seq/bwa_airseq_vs_PT
-srun zcat ${ASvsPHI}/*JIC*95*95*.sam.gz | grep -E "taxid\|208348\|" | grep -v "^@" | awk '{print $1}' > names.txt
-srun zcat ${reads}/*JIC*.fa.gz | grep -A1 -f names.txt --no-group-separator > ${prefix}.reads.fa
