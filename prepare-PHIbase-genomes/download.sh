6ded6c03a933fce67471bc5640da96893309ad8b
mgiolai <mgiolai.ac@gmail.com>
2024-06-25 17:08:37 +0200

    Update download.sh
    Changes to add human T2T reference to species database.

--- prepare-PHIbase-genomes/download.sh
+++ prepare-PHIbase-genomes/download.sh
@@ -38,3 +38,15 @@ for file in ${files[@]}; do
 	taxid=$(grep ${file} download.txt | awk -v FS='\t' '{print $2}')
 	gunzip -c ${file} | sed "s/^>/>taxid|${taxid}|/g" >> reference.fa
 done
+
+#Download and add human T2T genome to reference
+wget -c https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.fna.gz
+gunzip -c GCF_009914755.1_T2T-CHM13v2.0_genomic.fna.gz | sed "s/^>/>taxid|9606|/g" >> reference.fa
+
+#Count genome sizes by taxid
+if [ -f reference_genomesizes.txt ]; then rm reference_genomesizes.txt; fi
+for file in ${files[@]}; do echo ${file} && taxid=$(grep ${file} download.txt | awk -v FS='\t' '{print $2}') && nt=$(gunzip -c ${file} | grep -v "$>" | wc -c) && echo -e ${taxid}'\t'${nt}'\t'${file} >> reference_genomesizes.txt; done
+
+#Add human reference genome size
+nt=$(gunzip -c GCF_009914755.1_T2T-CHM13v2.0_genomic.fna.gz | grep -v "$>" | wc -c)
+echo -e '9606\t'${nt}'\tGCF_009914755.1_T2T-CHM13v2.0_genomic.fna.gz' >> reference_genomesizes.txt
